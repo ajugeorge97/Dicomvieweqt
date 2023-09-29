db414dca39b8a3343b535fd6932281af378fe60c
Aju George <aju.george@fau.de>
2023-09-29 14:57:35 +0200

    added padding to align the widget dim

--- widget.py
+++ widget.py
@@ -43,39 +43,30 @@ class Widget(QWidget):
 
     def handle_image_viewer(self,viewer):
         viewer.setStyleSheet("background-color:#808080;")
+        data=self.handle_dicom(path="Sampledata/series-000001/image-000001.dcm")
 
         self.QHBoxLayout_viewer=QHBoxLayout(viewer)
         self.QHBoxLayout_viewer.setContentsMargins(0,0,0,0)
-        self.image_viewer=FigureCanvas(Figure(figsize=(10, 3),facecolor='black'))
+        self.image_viewer=FigureCanvas(Figure(layout='tight',facecolor='black'))
         # Ideally one would use self.addToolBar here, but it is slightly
+
         # incompatible between PyQt6 and other bindings, so we just add the
         # toolbar as a plain widget instead.
         #self.QHBoxLayout_viewer.addWidget(NavigationToolbar(self.image_viewer, self))
         self.QHBoxLayout_viewer.addWidget(self.image_viewer)
         tbar=NavigationToolbar(self.image_viewer)
         tbar.pan()
+        left_pad = 115
+        right_pad = 115
+
+        # Pad the array
+        padded_array = np.pad(data.pixel_array, ((0, 0), (left_pad, right_pad)), mode='constant', constant_values=0)
+
+        print(padded_array)
 
         self._static_ax = self.image_viewer.figure.subplots()
-        t = np.linspace(0, 10, 501)
-        data=self.handle_dicom(path="Dicomvieweqt/Sampledata/series-000001/image-000001.dcm")
-        self._static_ax.imshow(data.pixel_array, cmap=plt.cm.gray)
+        self._static_ax.imshow(padded_array, cmap=plt.cm.gray,aspect=None)
         self._static_ax.axis('off')
-
-
-        #sizepolicy=QSizePolicy(QSizePolicy.Maximum,QSizePolicy.Maximum)
-        #sizepolicy.setHorizontalPolicy(QSizePolicy.Maximum)
-        #sizepolicy.setVerticalPolicy(QSizePolicy.Maximum)
-        #sizepolicy.setHorizontalStretch(0)
-        #sizepolicy.setVerticalStretch(0)
-        #sizepolicy.setHeightForWidth(self.image_viewer.sizePolicy().hasHeightForWidth())
-        #self.image_viewer.setSizePolicy(sizepolicy)
-
-        #self.image_viewer.setScaledContents(True)
-        #self.image_viewer.setFrameShape(QFrame.StyledPanel)
-        #self.image_viewer.setFrameShadow(QFrame.Raised)
-        self.image_viewer.setStyleSheet("background-color:#ffffff;")
-        image=QPixmap("Dicomvieweqt/Screenshot 2023-09-28 at 11.58.11.png")
-        #self.image_viewer.setPixmap(image)
 
 
     def initUI(self):
